8846eb378397f9044fdcce84a5123cc31fae1b4f
Dim31 <46557331+Dim31@users.noreply.github.com>
2019-05-21 15:29:57 +0200

    Add files via upload

---
+++ demandeurs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FREDI_TRESORIER.Classe
+{
+    class demandeurs
+    {
+        private string adresse_mail;
+        private string nom;
+        private string prenom;
+        private string rue;
+        private int cp;
+        private string ville;
+        private string password;
+
+        public demandeurs(string adresse_mail, string nom, string prenom, string rue, int cp, string ville, string password)
+        {
+            this.adresse_mail = adresse_mail;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.rue = rue;
+            this.cp = cp;
+            this.ville = ville;
+            this.password = password;
+        }
+        public demandeurs() { }
+
+        public string Adresse_mail { get => adresse_mail; set => adresse_mail = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Rue { get => rue; set => rue = value; }
+        public int Cp { get => cp; set => cp = value; }
+        public string Ville { get => ville; set => ville = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
---
+++ ligue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FREDI_TRESORIER.Classe
+{
+    class ligue
+    {
+        private int num_ligue;
+        private string nom;
+        private string sigle;
+        private string president;
+
+        public ligue(int num_ligue, string nom, string sigle, string president)
+        {
+            this.Num_ligue = num_ligue;
+            this.Nom = nom;
+            this.Sigle = sigle;
+            this.President = president;
+        }
+
+        public ligue() { }
+        public int Num_ligue { get => num_ligue; set => num_ligue = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Sigle { get => sigle; set => sigle = value; }
+        public string President { get => president; set => president = value; }
+    }
+}
